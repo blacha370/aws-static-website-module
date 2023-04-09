778599f4ee20484bc012df7ecd0ec5e91a3c1f3d
blacha370 <lukasz.blacha370@gmail.com>
2023-04-09 17:43:01 +0200

    Add conditional basic auth credentials generation
    Signed-off-by: blacha370 <lukasz.blacha370@gmail.com>

--- lambda.tf
+++ lambda.tf
@@ -5,9 +5,23 @@ data "archive_file" "basic_auth" {
   type        = "zip"
 
   source {
-    content  = templatefile(format("%s/basic-auth-lambda/index.js.tftpl", path.module), { username = "username", password = "password" })
+    content  = templatefile(format("%s/basic-auth-lambda/index.js.tftpl", path.module), { username = local.basic_auth_user , password = local.basic_auth_password })
     filename = "index.js"
   }
+}
+
+resource "random_string" "basic_auth_user" {
+  count = local.create_basic_auth_user ? 1 : 0
+
+  length  = 8
+  special = false
+}
+
+resource "random_password" "basic_auth_password" {
+  count = local.create_basic_auth_password ? 1 : 0
+
+  length  = 16
+  special = true
 }
 
 resource "aws_lambda_function" "basic_auth" {
--- locals.tf
+++ locals.tf
@@ -1,3 +1,9 @@
 locals {
   prefix = format("%s-%s", var.project, terraform.workspace)
+
+  create_basic_auth_user     = var.enable_basic_auth && var.basic_auth_user == ""
+  create_basic_auth_password = var.enable_basic_auth && var.basic_auth_password == ""
+
+  basic_auth_user     = local.create_basic_auth_user ? random_string.basic_auth_user[0].result : var.basic_auth_user
+  basic_auth_password = local.create_basic_auth_password ? nonsensitive(random_password.basic_auth_password[0].result) : var.basic_auth_password
 }
