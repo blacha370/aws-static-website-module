72953447a868dfbbe2fccec808ee4dcaa7d67976
blacha370 <lukasz.blacha370@gmail.com>
2023-04-09 17:42:09 +0200

    Add variables
    Signed-off-by: blacha370 <lukasz.blacha370@gmail.com>

--- variables.tf
+++ variables.tf
@@ -1,7 +1,24 @@
+####################################
+#       Project
+####################################
 variable "project" {
   type = string
 }
 
+####################################
+#       Basic auth
+####################################
 variable "enable_basic_auth" {
   type = bool
+  default = false
+}
+
+variable "basic_auth_user" {
+  type    = string
+  default = ""
+}
+
+variable "basic_auth_password" {
+  type    = string
+  default = ""
 }
